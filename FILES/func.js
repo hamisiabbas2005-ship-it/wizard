5ac7fac6716d60e4079fd98583fcac4c22499e6c
HAMISI <hamisiabbas2005@gmail.com>
2025-09-20 07:39:02 +0000

    hamisi

---
+++ FILES/func.js
@@ -0,0 +1,15 @@
+//i'm declaring my function here 
+function greetUser(name){
+console.log(`hallo ${name}!`);
+}
+
+// i'm declaring my call function
+greetUser("david");
+
+//I'm declaring my function 
+function greet(){
+console.log("welcome to kenya")
+}
+
+//This is my call function 
+greet()
---
+++ FILES/greet.js
@@ -0,0 +1,7 @@
+//Im declaring my function 
+function greet(){
+console.log("welcome to kenya")
+}
+
+//This is my call function 
+greet()
