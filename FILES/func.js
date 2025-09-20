1a6355e7c8ec501c80c2fdbd9b738e9c3073088e
HAMISI <hamisiabbas2005@gmail.com>
2025-09-20 08:58:18 +0000

    123

--- FILES/func.js
+++ FILES/func.js
@@ -13,3 +13,27 @@ console.log("welcome to kenya")
 
 //This is my call function 
 greet()
+
+//This is my call function 
+function sum(a, b){
+let result = a * b;
+return result;
+}
+
+// i'm declaring my call function
+let result = sum(4, 3);
+console.log(result);
+
+//i'm declaring my function here 
+function check(number){
+if (number % 2 === 0){
+console.log(`${number}is even`);
+} else {
+console.log(`${number}is odd`);
+}
+}
+
+
+//This is my call function 
+check (4)
+check(7)
